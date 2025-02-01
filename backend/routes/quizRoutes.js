32084ba4d93716a60a9385df0f4db5292f48fc6b
Syed Talha <syedtalhadev7@gmail.com>
2025-02-01 16:02:58 +0500

    Fix quiz fetching API and update routes

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createQuiz, getQuizzes } = require("../controllers/quizController");
+const { createQuiz, getQuizzes, getQuizById } = require("../controllers/quizController");
 const router = express.Router();
 
 router.post("/", createQuiz);
 router.get("/", getQuizzes);
+router.get("/:id", getQuizById);
 
 module.exports = router;
