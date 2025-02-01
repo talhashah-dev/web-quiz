101d4880f24db446ec5c2f29c70cf02d642d30e8
Syed Talha <syedtalhadev7@gmail.com>
2025-02-02 01:15:44 +0500

    redesign landing page

---
+++ frontend/src/assets/export.js
@@ -0,0 +1,7 @@
+import HeroImg from "./coding_boy.svg";
+import HTML from "./html.svg";
+import CSS from "./css.svg";
+import JavaScript from "./javascript.svg";
+import Python from "./python.svg";
+
+export { HeroImg, HTML, CSS, JavaScript, Python };
